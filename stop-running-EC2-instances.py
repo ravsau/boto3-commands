c067f4bdcbebd756a45e2638f2cf75f20a8bd417
saurav <saurav@Robs-MacBook-Pro.local>
2018-02-10 16:41:14 -0500

    remote lambda reference

--- stop-running-EC2-instances.py
+++ stop-running-EC2-instances.py
@@ -3,22 +3,22 @@ import boto3
 
 client=boto3.client('ec2')
 
-def lambda_handler(event, context):
-
-    response=client.describe_instances()
 
 
-    for reservation in response["Reservations"]:
-
-        for instance in reservation["Instances"]:
-
-           print(instance["InstanceId"] + "stopping")
-
-           id=[instance["InstanceId"]]
-
-           client.stop_instances(InstanceIds=id)
+response=client.describe_instances()
 
 
+for reservation in response["Reservations"]:
+
+	for instance in reservation["Instances"]:
+
+        	print(instance["InstanceId"] + "stopping")
+
+        	id=[instance["InstanceId"]]
+
+        	client.stop_instances(InstanceIds=id)
 
 
-    return("Completed")
+
+
+

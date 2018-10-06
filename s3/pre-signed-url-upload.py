d1e16477f456bb336ac048bb7b95bb5dceec7cd8
Saurav Sharma <sauravsharma011@gmail.com>
2018-10-06 13:46:24 -0400

    Update pre-signed-url-upload.py

--- s3/pre-signed-url-upload.py
+++ s3/pre-signed-url-upload.py
@@ -1,6 +1,16 @@
 import boto3
 s3 = boto3.client('s3')
 
+"""
+If you don't have CLI and therefore AWS Credentials setup , then un-comment the lines below and remove the line above. Add your AWS credentials.
+
+s3 = boto3.client(
+   's3',
+   aws_access_key_id='AKIAIO5FODNN7EXAMPLE',
+   aws_secret_access_key='ABCDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE'
+) 
+"""
+
 bucket = raw_input("Enter your Bucket Name: ")
 key= raw_input("Enter your desired filename/key for this upload: ")
 
